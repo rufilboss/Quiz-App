e656125b68ed0abf0dc97181d51baefe91e74228
Ilyas Rufai <rufaiilyas@gmail.com>
2024-06-04 15:17:48 +0100

    update

--- main.go
+++ main.go
@@ -15,16 +15,15 @@ func problemPuller(fileName string) ([]problem, error) {
 		// Create a new reader
 		csvR := csv.NewReader(fObj)
 		// Read file contents
-		if clines, err := csvR.ReadAll(); err == nil {
-            return parseProblem(clines), nil
+		if cLines, err := csvR.ReadAll(); err == nil {
+            // call the function
+            return parseProblem(cLines), nil
         } else {
             return nil, fmt.Errorf("failed to read data in csv" + "format from %s file; %s", fileName, err.Error())
         }
 	} else {
         return nil, fmt.Errorf("error opening %s file; %s", fileName, err.Error())
     }
-
-	//
 }
 
 func main() {
@@ -44,8 +43,36 @@ func main() {
 	// Initailize the timer
 	tObj := time.NewTimer(time.Duration(*timer) * time.Second)
 	ansC := make(chan string)
-	// Loop through the problems and get answers
-	// Calculate and print out results
+
+// Loop through the problems and get answers
+problemLoop:
+
+    for i, p := range problems{
+        var answer string
+        fmt.Printf("Problem %d: %s=\n", i+1, p.q)
+
+        go func() {
+            fmt.Scanf("%s", &answer)
+            ansC <- answer
+        }()
+        select{
+        case <- tObj.C:
+            fmt.Println()
+            break problemLoop
+        case iAns := <- ansC:
+            if iAns == p.a{
+                correctAns++
+            }
+            if i== len(problems)-1 {
+                close(ansC)
+            }
+        }
+        // Calculate and print out results
+        fmt.Printf("Your results is %d out of %d\n", correctAns, len(problems))
+        fmt.Printf("Press enter key to exit")
+        <-ansC
+
+    }
 }
 
 func parseProblem(lines [][]string) []problem {
@@ -54,9 +81,15 @@ func parseProblem(lines [][]string) []problem {
     for i := 0 ; i < len(lines) ; i++ {
         r[i] = problem{q: lines[i][0], a: lines[i][1]}
     }
+    return r
 }
 
 type problem struct {
 	q string
 	a string
+}
+
+func exit(msg string){
+    fmt.Println(msg)
+    os.Exit(1)
 }
